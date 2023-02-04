060d22cbede255f0d60b75f20a3b972d9ff09bb6
Hesam760 <ghasemi.hesam3@gmail.com>
2023-02-04 11:55:54 +0330

    show output in GUI

--- elevator.py
+++ elevator.py
@@ -3,139 +3,107 @@ from threading import Thread
 from queue import PriorityQueue
 import time
 import PySimpleGUI as sg
-disk_size = 200
 
-
-# Function to perform C-LOOK on the request
-# array starting from the given head
-def cLOOK(arr, head):
+def staticProcess(arrayInput, head):
     size = 4
     seek_count = 0
     distance = 0
-    cur_track = 0
-    array = arr
+    newFloor = 0
+    array = arrayInput
     down = []
     up = []
     sub_array = []
-    seek_sequence = []
+    seqArray = []
     direction_up = True
 
-    len_check = math.ceil(len(arr) / size)
+    len_check = math.ceil(len(arrayInput) / size)
     if len_check < 1:
         len_check = 1
 
-    if len(arr) < size:
+    if len(array) < size:
         size = len(array)
-    for k in range(0, len_check):
-        for i in range(0, size):
-            if len(array) == 0:
-                break
-            item = array.pop(0)
-            # sub_array.append(item)
-            if item < head:
-                down.append(item)
-            if item > head:
-                up.append(item)
+        
+    
+    if len(array) != 1 :
+        for k in range(0, len_check):
+            for i in range(0, size):
+                if len(array) == 0:
+                    break
+                item = array.pop(0)
+                if item < head:
+                    down.append(item)
+                if item > head:
+                    up.append(item)
 
-        sub_array = up + down
-        sub_array.sort()
-        print("array :", sub_array)
-        down.sort()
-        up.sort()
+            sub_array = up + down
+            sub_array.sort()
+            # print("array :", sub_array)
+            down.sort()
+            up.sort()
 
-        print("up:", up)
-        print("down", down)
+            if (k == 0 and len(up) >= len(down)) or (sub_array[0] > head) or (direction_up is True and sub_array[3] > head):
+                for i in range(len(up)):
+                    direction_up = True
+                    newFloor = up[i]
 
-        if (k == 0 and len(up) >= len(down)) or (sub_array[0] > head) or (direction_up is True and sub_array[3] > head):
-            for i in range(len(up)):
-                direction_up = True
-                cur_track = up[i]
+                    seqArray.append(newFloor)
+                    
+                    distance = abs(newFloor - head)
+                                        
+                    head = newFloor
 
-                # Appending current track
-                # seek sequence
-                seek_sequence.append(cur_track)
+                if len(down) != 0:
+                    seek_count += abs(head - down[0])
+                    down.reverse()
+                    head = down[0]
 
-                # Calculate absolute distance
-                distance = abs(cur_track - head)
+                    for i in range(len(down)):
+                        direction_up = False
+                        newFloor = down[i]
+                        
+                        seqArray.append(newFloor)
 
-                # Increase the total count
-                seek_count += distance
+                        distance = abs(newFloor - head)
 
-                # Accessed track is now new head
-                head = cur_track
+                        head = newFloor
 
-            if len(down) != 0:
-                seek_count += abs(head - down[0])
-                down.reverse()
-                head = down[0]
-                # Now service the requests again
-                # which are down
-                for i in range(len(down)):
+            elif sub_array[0] < head and direction_up is False:
+                for i in range(0, len(down)):
                     direction_up = False
-                    cur_track = down[i]
+                    newFloor = down[i]
+                    
+                    seqArray.append(newFloor)
 
-                    # Appending current track to
-                    # seek sequence
-                    seek_sequence.append(cur_track)
+                    distance = abs(newFloor - head)
 
-                    # Calculate absolute distance
-                    distance = abs(cur_track - head)
-
-                    # Increase the total count
                     seek_count += distance
 
-                    # Accessed track is now the new head
-                    head = cur_track
+                    head = newFloor
 
-        elif sub_array[0] < head and direction_up is False:
-            # Now service the requests again
-            # which are down
-            for i in range(0, len(down)):
-                direction_up = False
-                cur_track = down[i]
+                for i in range(len(up)):
+                    direction_up = True
+                    newFloor = up[i]
 
-                # Appending current track to
-                # seek sequence
-                seek_sequence.append(cur_track)
+                    seqArray.append(newFloor)
 
-                # Calculate absolute distance
-                distance = abs(cur_track - head)
+                    distance = abs(newFloor - head)
 
-                # Increase the total count
-                seek_count += distance
+                    head = newFloor
 
-                # Accessed track is now the new head
-                head = cur_track
+            for i in range(0 , len(seqArray)):
+                print(seqArray[i])
+            print('--------------')
 
-            for i in range(len(up)):
-                direction_up = True
-                cur_track = up[i]
+            up.clear()
+            down.clear()
+            sub_array.clear()
+            seqArray.clear()
+    else :
+        print(array[0])
 
-                # Appending current track
-                # seek sequence
-                seek_sequence.append(cur_track)
-
-                # Calculate absolute distance
-                distance = abs(cur_track - head)
-
-                # Increase the total count
-                seek_count += distance
-
-                # Accessed track is now new head
-                head = cur_track
-
-        for i in range(len(seek_sequence)):
-            print(seek_sequence[i])
-
-        up.clear()
-        down.clear()
-        sub_array.clear()
-        seek_sequence.clear()
 
 
 staticList = []
-
-
 def static(head):
     numReq = int(input("number of request : \n"))
     count = 0
@@ -144,135 +112,219 @@ def static(head):
         staticList.append(inputStatic)
         count += 1
     print(staticList)
-    cLOOK(staticList, int(head))
+    staticProcess(staticList, int(head))
 
 
 dynamicList = []
 wait = False
 
-
+priority = []
+inputList = []
 def thread():
     sg.theme("GreenTan")
-    layout = [
-          [sg.Text('Internal Requests')],
-          [sg.Button('1' , size = (2,1)) ,  sg.Button('2', size = (2,1)), sg.Button('3' , size = (2,1))],
-          [sg.Button('4', size = (2,1)), sg.Button('5',size = (2,1)), sg.Button('6' ,size = (2,1))],
-          [sg.Button('7', size = (2,1)), sg.Button('8' , size = (2,1)), sg.Button('9', size = (2,1))],
-          [sg.Button('10', size = (2,1)), sg.Button('11', size = (2,1)), sg.Button('12' , size = (2,1))],
-          [sg.Button('13', size = (2,1)), sg.Button('14', size = (2,1)), sg.Button('15', size = (2,1))],
-          [sg.Text('-'* 30)],
-          [sg.Text('External Requests')],
-          [sg.Button('1F' , size = (2,2)) ,  sg.Button('2F', size = (2,2)), sg.Button('3F' , size = (2,2))],
-          [sg.Button('4F', size = (2,2)), sg.Button('5F',size = (2,2)), sg.Button('6F' ,size = (2,2))],
-          [sg.Button('7F', size = (2,2)), sg.Button('8F' , size = (2,2)), sg.Button('9F', size = (2,2))],
-          [sg.Button('10F', size = (2,2)), sg.Button('11F', size = (2,2)), sg.Button('12F' , size = (2,2))],
-          [sg.Button('13F', size = (2,2)), sg.Button('14F', size = (2,2)), sg.Button('15F', size = (2,2))]
-        ]
+    
+    col = sg.Col([     
+          [sg.Text(size=(100, 6), key='-OUT-')],
+          [sg.Text(size=(100, 6), key='-OUTP-')],
+          [sg.Text('Internal Requests                        External Reqests')],
+          [
+            sg.Button('1' , size = (4,2)) ,  sg.Button('2', size = (4,2)), sg.Button('3' , size = (4,2)) , sg.Text('        ') , 
+            sg.Button('1F' , size = (4,2)) ,  sg.Button('2F', size = (4,2)), sg.Button('3F' , size = (4,2))       
+          ],
+          
+          [
+            sg.Button('4', size = (4,2)), sg.Button('5',size = (4,2)), sg.Button('6' ,size = (4,2)) , sg.Text('        ')  , 
+            sg.Button('4F', size = (4,2)), sg.Button('5F',size = (4,2)), sg.Button('6F' ,size = (4,2))
+          ],
+          
+          [
+            sg.Button('7', size = (4,2)), sg.Button('8' , size = (4,2)), sg.Button('9', size = (4,2)) , sg.Text('        ') ,
+            sg.Button('7F', size = (4,2)), sg.Button('8F' , size = (4,2)), sg.Button('9F', size = (4,2))
+           ],
+          
+          [
+            sg.Button('10', size = (4,2)), sg.Button('11', size = (4,2)), sg.Button('12' , size = (4,2)), sg.Text('        ') ,
+            sg.Button('10F', size = (4,2)), sg.Button('11F', size = (4,2)), sg.Button('12F' , size = (4,2))
+          ],
+          [
+            sg.Button('13', size = (4,2)), sg.Button('14', size = (4,2)), sg.Button('15', size = (4,2)), sg.Text('        ') ,
+            sg.Button('13F', size = (4,2)), sg.Button('14F', size = (4,2)), sg.Button('15F', size = (4,2))
+          ]], size=(800,600), pad=(0,0))
+
+    layout = [[sg.Text('Elevator')],
+                [sg.Frame('', [[col]])]]
 
     window = sg.Window('Elevator').Layout(layout)
     while True:
         button , values = window.Read(timeout = 100)
-            # in1 = input("enter request\n")
+        window['-OUT-'].update(inputList)
+        # if len(priority[1]) != 0 :
+        window['-OUTP-'].update(priority)
         match button:
             case '1' : 
                 t1 = time.time()
-                dynamicList.append(['1 Internal', t1])        
+                dynamicList.append(['1 Internal', t1])
+                inputList.append('1 Internal')        
             case '2' :
                 t1 = time.time()
                 dynamicList.append(['2 Internal',t1])
+                inputList.append('2 Internal')  
             case '3' :
                 t1 = time.time()
                 dynamicList.append(['3 Internal',t1])
+                inputList.append('3 Internal')  
             case '4' :
                 t1 = time.time()
                 dynamicList.append(['4 Internal',t1])
+                inputList.append('4 Internal')  
             case '5' :
                 t1 = time.time()
                 dynamicList.append(['5 Internal',t1])
+                inputList.append('5 Internal')  
             case '6' :
                 t1 = time.time()
                 dynamicList.append(['6 Internal',t1])
+                inputList.append('6 Internal')  
             case '7' :
                 t1 = time.time()
                 dynamicList.append(['7 Internal',t1])
+                inputList.append('7 Internal')  
             case '8' : 
                 t1 = time.time()
                 dynamicList.append(['8 Internal',t1])
+                inputList.append('8 Internal')  
             case '9' : 
                 t1 = time.time()
                 dynamicList.append(['9 Internal',t1])
+                inputList.append('9 Internal')  
             case '10' :
                 t1 = time.time()
                 dynamicList.append(['10 Internal',t1])
+                inputList.append('10 Internal')  
             case '11' :
                 t1 = time.time()
                 dynamicList.append(['11 Internal',t1])
+                inputList.append('11 Internal')  
             case '12' : 
                 t1 = time.time()
                 dynamicList.append(['12 Internal',t1])
+                inputList.append('12 Internal')  
             case '13' :
                 t1 = time.time()
                 dynamicList.append(['13 Internal',t1])
+                inputList.append('13 Internal')  
             case '14' :
                 t1 = time.time()
                 dynamicList.append(['14 Internal',t1])
+                inputList.append('14 Internal')  
             case '15' :
                 t1 = time.time()
                 dynamicList.append(['15 Internal',t1])
+                inputList.append('15 Internal')  
             case '1F' : 
                 t1 = time.time()
-                dynamicList.append(['1 External', t1])        
+                dynamicList.append(['1 External', t1])
+                inputList.append('1 External')          
             case '2F' :
                 t1 = time.time()
                 dynamicList.append(['2 External',t1])
+                inputList.append('2 External')  
             case '3F' :
                 t1 = time.time()
                 dynamicList.append(['3 External',t1])
+                inputList.append('3 External')  
             case '4F' :
                 t1 = time.time()
                 dynamicList.append(['4 External',t1])
+                inputList.append('4 External')  
             case '5F' :
                 t1 = time.time()
                 dynamicList.append(['5 External',t1])
+                inputList.append('5 External')  
             case '6F' :
                 t1 = time.time()
                 dynamicList.append(['6 External',t1])
+                inputList.append('6 External')  
             case '7F' :
                 t1 = time.time()
                 dynamicList.append(['7 External',t1])
+                inputList.append('7 External')  
             case '8F' : 
                 t1 = time.time()
                 dynamicList.append(['8 External',t1])
+                inputList.append('8 External')  
             case '9F' : 
                 t1 = time.time()
                 dynamicList.append(['9 External',t1])
+                inputList.append('9 External')  
             case '10F' :
                 t1 = time.time()
                 dynamicList.append(['10 External',t1])
+                inputList.append('10 External')  
             case '11F' :
                 t1 = time.time()
                 dynamicList.append(['11 External',t1])
+                inputList.append('11 External')  
             case '12F' : 
                 t1 = time.time()
                 dynamicList.append(['12 External',t1])
+                inputList.append('12 External')  
             case '13F' :
                 t1 = time.time()
                 dynamicList.append(['13 External',t1])
+                inputList.append('13 External')  
             case '14F' :
                 t1 = time.time()
                 dynamicList.append(['14 External',t1])
+                inputList.append('14 External')  
             case '15F' :
                 t1 = time.time()
                 dynamicList.append(['15 External',t1])
+                inputList.append('15 External')  
+        
+        # if button == '1' :
+        #     t1 = time.time()
+        #     dynamicList.append(['1 Internal',t1])
+        # if button == '2' :
+        #     t1 = time.time()
+        #     dynamicList.append(['2 Internal',t1])
+        # if button == '3' :
+        #     t1 = time.time()
+        #     dynamicList.append(['3 Internal',t1])
+        # if button == '4' :
+        #     t1 = time.time()
+        #     dynamicList.append(['4 Internal',t1])
+        # if button == '5' :
+        #     t1 = time.time()
+        #     dynamicList.append(['5 Internal',t1])
+        # if button == '6' :
+        #     t1 = time.time()
+        #     dynamicList.append(['6 Internal',t1])
+        # if button == '7' :
+        #     t1 = time.time()
+        #     dynamicList.append(['7 Internal',t1])    
+        # if button == '8' :
+        #     t1 = time.time()
+        #     dynamicList.append(['8 Internal',t1])
+        # if button == '9' :
+        #     t1 = time.time()
+        #     dynamicList.append(['9 Internal',t1])
+        # if button == '10' :
+        #     t1 = time.time()
+        #     dynamicList.append(['10 Internal',t1])
+        # if button == '11' :
+        #     t1 = time.time()
+        #     dynamicList.append(['11 Internal',t1])
+        # if button == '12' :
+        #     t1 = time.time()
+        #     dynamicList.append(['12 Internal',t1])
+        # if button == '13' :
+        #     t1 = time.time()
+        #     dynamicList.append(['13 Internal',t1])
+            
         if wait:
               time.sleep(5)
 
-    # while True:
-    #     in1 = input("enter request\n")
-    #     t1 = time.time()
-    #     dynamicList.append([in1, t1])
-    #     if wait:
-    #         time.sleep(5)
 
 def dynamic(head):
     t1 = Thread(target=thread, args=())
@@ -295,9 +347,13 @@ def dynamic(head):
         sizeQ = q1.qsize()
         for i in range(0, sizeQ):
             head = q1.get()[1]
+            priority.append(head)
             time.sleep(1)
+            # print("headr is :" , header)
             print("floor is now :\n", head)
             dynamicList.pop(0)
+            if i == sizeQ - 1:
+                priority.append('---')
         # dynamicList.clear()
         if q1.empty():
             wait = False
@@ -312,18 +368,19 @@ def update(dynamic, head, direction):
         sp = i[0].split()
         print("sp is :" , sp)
         # print("i is :\n", i )
-        difference = abs(head - int(sp[0]))
+        difference = abs(int(head) - int(sp[0]))
         diff = now - i[1]
         d1 = difference / diff
-        if (head - int(sp[0]) > 0 and direction) or (head - int(sp[0]) < 0 and not direction):
+        if (head - int(sp[0]) > 0 and direction) or (int(head) - int(sp[0]) < 0 and not direction):
             d1 *= 0.7
         if sp[1] == 'Internal' :
-            d1 *= (1/3)
+            d1 *= (1/2)
         list1.append(d1)
         index.append(int(sp[0]))
         # print("list1 is :\n" , list1)
     for i in range(0, len(list1)):
         q.put((list1[i], index[i]))
+        # priority.append(("floor :" , index[i]))
         print("printed :" , (list1[i], index[i]))
     list1.clear()
     q1 = q
@@ -336,6 +393,6 @@ if __name__ == "__main__":
     if alg == '1':
         headStatic = input("enter the current floor of elevator\n")
         static(headStatic)
-    if alg == '2':
+    if alg == '2':   
         headDynamic = int(input("enter the current floor of elevator\n"))
         dynamic(headDynamic)
