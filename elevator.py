1264df1e8dcc35bf3a15f401af4de599cc8afae7
Hesam760 <ghasemi.hesam3@gmail.com>
2023-02-09 01:39:00 +0330

    Last Update

--- elevator.py
+++ elevator.py
@@ -42,17 +42,13 @@ def staticProcess(arrayInput, head):
                 sub_array.sort()
                 down.sort()
                 up.sort()
-                print("up :" , up)
-                print("downL " , down)
                     
-                if len(k == 0 and len(up) >= len(down)) or (sub_array[0] > head) or (direction_up is True and sub_array[len(down) + len(up) - 1] > head) :
+                if k == 0 and len(up) >= len(down) or (sub_array[0] > head) or (direction_up is True and sub_array[len(down) + len(up) - 1] > head) :
                     for i in range(len(up)):
                         direction_up = True
                         newFloor = up[i]
 
                         seqArray.append(newFloor)
-                        
-                        distance = abs(newFloor - head)
                                             
                         head = newFloor
 
@@ -67,12 +63,10 @@ def staticProcess(arrayInput, head):
                             
                             seqArray.append(newFloor)
 
-                            distance = abs(newFloor - head)
-
                             head = newFloor
 
                 elif (sub_array[0] < head and direction_up is False) or (len(down) + len(up) <= 4):
-                    if direction_up and head > down[len(down) - 1] and head > down[0] :
+                    if direction_up and head > down[len(down) - 1] and head > down[0]:
                         down.reverse()
                     for i in range(0, len(down)):
                         direction_up = False
@@ -119,8 +113,11 @@ def static(head):
     count = 0
     while count != numReq:
         inputStatic = int(input('Enter the requests :'))
-        staticList.append(inputStatic)
-        count += 1
+        if inputStatic >= 0 and inputStatic <= 15:
+            staticList.append(inputStatic)
+            count += 1
+        else : 
+            print('invalid input please change it ')     
     print(staticList)
     staticProcess(staticList, int(head))
 
@@ -176,143 +173,15 @@ def thread():
         window['-OUT-'].update(inputList)
         if len(priority) != 0 :
             while count < len(priority) :
-                inList.append(priority[count])
-                # checkList.append(priority[count])
-                # for checkList[count] in checkList :
-                #     countinue
-                # for check in checkList:
+                if count >= 1 and priority[count] != priority[count - 1]:
+                    inList.append(priority[count])
+                elif count == 0 :
+                    inList.append(priority[count])
                 
-                # if checkList[count] in checkList and checkList[count] != '--Next Request--':
-                #     continue 
-                # else :
-                # inList.append(checkList[count])
-                # inList.append(priority[count]) 
                 window['-OUTP-'].update(inList)
                 count += 1
-            checkList = []
-         
-        if button == '1' :
-            t1 = time.time()
-            dynamicList.append(['1 Internal',t1])
-            inputList.append('1 Internal')        
-        elif button == '2' :
-            t1 = time.time()
-            dynamicList.append(['2 Internal',t1])
-            inputList.append('2 Internal')        
-        elif button == '3' :
-            t1 = time.time()
-            dynamicList.append(['3 Internal',t1])
-            inputList.append('3 Internal')        
-        elif button == '4' :
-            t1 = time.time()
-            dynamicList.append(['4 Internal',t1])
-            inputList.append('4 Internal')        
-        elif button == '5' :
-            t1 = time.time()
-            dynamicList.append(['5 Internal',t1])
-            inputList.append('5 Internal')        
-        elif button == '6' :
-            t1 = time.time()
-            dynamicList.append(['6 Internal',t1])
-            inputList.append('6 Internal')      
-        elif button == '7' :
-            t1 = time.time()
-            dynamicList.append(['7 Internal',t1])    
-            inputList.append('7 Internal')        
-        elif button == '8' :
-            t1 = time.time()
-            dynamicList.append(['8 Internal',t1])
-            inputList.append('8 Internal')        
-        elif button == '9' :
-            t1 = time.time()
-            dynamicList.append(['9 Internal',t1])
-            inputList.append('9 Internal')       
-        elif button == '10' :
-            t1 = time.time()
-            dynamicList.append(['10 Internal',t1])
-            inputList.append('10 Internal')        
-        elif button == '11' :
-            t1 = time.time()
-            dynamicList.append(['11 Internal',t1])
-            inputList.append('11 Internal')        
-        elif button == '12' :
-            t1 = time.time()
-            dynamicList.append(['12 Internal',t1])
-            inputList.append('12 Internal')       
-        elif button == '13' :
-            t1 = time.time()
-            dynamicList.append(['13 Internal',t1])
-            inputList.append('13 Internal')  
-        elif button == '14' :
-            t1 = time.time()
-            dynamicList.append(['13 Internal',t1])
-            inputList.append('13 Internal')
-        elif button == '15' :
-            t1 = time.time()
-            dynamicList.append(['13 Internal',t1])
-            inputList.append('13 Internal')    
-        elif button == '1F':
-            t1 = time.time()
-            dynamicList.append(['1 External',t1])
-            inputList.append('1 Externa;')  
-        elif button == '2F':
-            t1 = time.time()
-            dynamicList.append(['2 External',t1])
-            inputList.append('2 External')  
-        elif button == '3F':
-            t1 = time.time()
-            dynamicList.append(['3 External',t1])
-            inputList.append('3 External')    
-        elif button == '4F':
-            t1 = time.time()
-            dynamicList.append(['4 External',t1])
-            inputList.append('4 External')  
-        elif button == '5F':
-            t1 = time.time()
-            dynamicList.append(['5 External',t1])
-            inputList.append('5 External')  
-        elif button == '6F':
-            t1 = time.time()
-            dynamicList.append(['6 External',t1])
-            inputList.append('6 External')  
-        elif button == '7F':
-            t1 = time.time()
-            dynamicList.append(['7 External',t1])
-            inputList.append('7 External')  
-        elif button == '8F':
-            t1 = time.time()
-            dynamicList.append(['8 External',t1])
-            inputList.append('8 External') 
-        elif button == '9F':
-            t1 = time.time()
-            dynamicList.append(['9 External',t1])
-            inputList.append('9 External') 
-        elif button == '10F' :
-            t1 = time.time()
-            dynamicList.append(['10 External',t1])
-            inputList.append('10 External') 
-        elif button == '11F' :
-            t1 = time.time()
-            dynamicList.append(['11 External',t1])
-            inputList.append('11 External') 
-        elif button == '12F' :
-            t1 = time.time()
-            dynamicList.append(['12 External',t1])
-            inputList.append('12 External') 
-        elif button == '13F' :
-            t1 = time.time()
-            dynamicList.append(['13 External',t1])
-            inputList.append('13 External') 
-        elif button == '14F' :
-            t1 = time.time()
-            dynamicList.append(['14 External',t1])
-            inputList.append('14 External') 
-        elif button == '15F' :
-            t1 = time.time()
-            dynamicList.append(['15 External',t1])
-            inputList.append('15 External') 
-        
-        elif wait:
+            checkList = []        
+        if wait:
               time.sleep(5)
 
 
@@ -320,9 +189,7 @@ def dynamic(head):
     t1 = Thread(target=thread, args=())
     t1.start()
 
-    # global wait
     direction_up = True
-    # time.sleep(1)
     if len(dynamicList) != 0:
         first_req = dynamicList[0]
         first_req = first_req.split()
@@ -356,12 +223,12 @@ def update(dynamic, head, direction):
     now = time.time()
     for i in dynamic:
         sp = i[0].split()
-        print("sp is :" , sp)
+        # print("sp is :" , sp)
         # print("i is :\n", i )
         differenceFloor = abs(int(head) - int(sp[0]))
         differenceTime = now - i[1]
         d1 = differenceFloor / differenceTime
-        if (head - int(sp[0]) > 0 and direction) or (int(head) - int(sp[0]) < 0 and not direction):
+        if (int(sp[0])  - int(head) > 0 and direction) or (int(sp[0]) - int(head) < 0 and not direction):
             d1 *= 0.65
         if sp[1] == 'Internal' :
             d1 *= (1/2)
@@ -371,7 +238,8 @@ def update(dynamic, head, direction):
     for i in range(0, len(list1)):
         q.put((list1[i], index[i]))
         # priority.append(("floor :" , index[i]))
-        print("printed :" , (list1[i], index[i]))
+        print("priority : :" , (list1[i], index[i]))
+    # print('head floor is :' , head)
     list1.clear()
     q1 = q
     q = PriorityQueue()
@@ -382,7 +250,13 @@ if __name__ == "__main__":
     alg = input("Enter the algorithm : 1- static | 2- dynamic\n")
     if alg == '1':
         headStatic = input("enter the current floor of elevator\n")
-        static(headStatic)
+        if headStatic >= 0 and headStatic <= 15:
+            static(headStatic)
+        else : 
+            print('invalid input')
     if alg == '2':   
         headDynamic = int(input("enter the current floor of elevator\n"))
-        dynamic(headDynamic)
+        if headDynamic >= 0 and headDynamic <= 15:
+            dynamic(headDynamic)
+        else : 
+            print('invalid input')
